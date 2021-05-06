89889cc883d1654992d2398ccc5e0b8b28ece4fc
Denis Dudic <denis@dudyk.com>
2021-05-06 14:33:49 +0300

    Fix: Generated '/etc/nginx/conf.d/default.conf' from 1 containers

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ dirName=$(dirname "$0")
 path=$(cd "$dirName" && pwd)
 dockerComposeFile="$path/docker-compose.yml"
 
-alreadyRan=$(docker ps -f name=docker-nginx-proxy -q | wc -l)
+alreadyRan=$(docker ps -f name=docker_nginx_proxy -q | wc -l)
 
 if [ "$alreadyRan" == 1 ]
 then
-docker stop $(docker ps -f name=docker-nginx-proxy -q) && docker-compose -f "$dockerComposeFile" up -d
+docker stop $(docker ps -f name=docker_nginx_proxy -q) && docker-compose -f "$dockerComposeFile" up -d
 else
   docker-compose -f "$dockerComposeFile" up -d
 fi
